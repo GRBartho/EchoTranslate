ae98b2d82bc84c3f7b30990437501893f9f88731
GRBartho <gabrielbartholomay@gmail.com>
2023-06-25 19:58:12 -0300

    created auto play audio checkbox

--- core/gui/gui.py
+++ core/gui/gui.py
@@ -1,6 +1,5 @@
 import os
 import tkinter as tk
-import whisper
 
 class GUI:
     def __init__(self, root, audio_recorder, transcriber):
@@ -12,6 +11,8 @@ class GUI:
         self.stop_button = None
         self.text_box = None
         self.play_audio_button = None
+        self.auto_play_audio = None
+        self.auto_play_audio_bool = False
 
     def create_widgets(self):
         self.title_label = tk.Label(
@@ -69,6 +70,20 @@ class GUI:
         )
         self.play_audio_button.pack_forget()
 
+        self.auto_play_audio = tk.Checkbutton(
+            self.root,
+            text="Tocar áudio automaticamente",
+            command=self.activate_auto_play,
+            font=("Arial", 16),
+            fg="#ffffff",
+            relief=tk.RAISED,
+            variable=self.auto_play_audio_bool,
+            activeforeground="#ffffff",
+            padx=20,
+            pady=10,
+        )
+        self.auto_play_audio.pack(pady=10)
+
         self.text_box = tk.Text(self.root, height=10, width=50)
         self.text_box.pack_forget()
 
@@ -81,10 +96,13 @@ class GUI:
         self.translated_text = ""
         # Start recording
         self.audio_recorder.start_recording()
+    
+    def printThingy(self):
+        print(self.auto_play_audio_bool)
 
     def stop_recording(self):
+        self.printThingy()
         self.audio_recorder.stop_recording()
-
         # Save recording
         filename = "recording.wav"
         self.audio_recorder.save_recording(filename)
@@ -93,7 +111,10 @@ class GUI:
         self.stop_button.config(state=tk.DISABLED)
         self.record_button.config(state=tk.ACTIVE)
         self.play_audio_button.pack(pady=10)
-        self.play_audio_button.config(state=tk.ACTIVE)
+        if self.auto_play_audio_bool:
+            self.play_audio()
+        else:
+            self.play_audio_button.config(state=tk.ACTIVE)
         self.show_text()
 
     def transcribe_text(self):
@@ -118,6 +139,10 @@ class GUI:
         filename = self.transcriber.text_to_audio(text, "translated_recording.mp3", 'en')
         # Start the audio file
         os.system("mpg123 " + filename)
+
+    def activate_auto_play(self):
+        self.auto_play_audio_bool = not self.auto_play_audio_bool
+        self.printThingy()
 
     def run(self):
         self.root.title("EchoTranslate")
