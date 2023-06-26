51c3090a0847dd6d8b513f5dc22179f694207f77
Ricardo Pacheco <ricardogpacheco999@gmail.com>
2023-06-25 22:43:54 -0300

    Methods descriptions, comments, colors, and click functions

--- core/gui/gui.py
+++ core/gui/gui.py
@@ -1,70 +1,78 @@
 import os
 import tkinter as tk
+from tkmacosx import Button
+import time
 
 class GUI:
     def __init__(self, root, audio_recorder, transcriber):
-        self.root = root
+        # Logic
         self.audio_recorder = audio_recorder
         self.transcriber = transcriber
         self.translated_text = ""
+        self.should_auto_play_audio = False
+        # Layout
+        self.root = root
         self.record_button = None
         self.stop_button = None
         self.text_box = None
         self.play_audio_button = None
         self.auto_play_audio = None
-        self.auto_play_audio_bool = False
+        # Language
+        self.input_language = "pt"
+        self.output_language = "en"
 
     def create_widgets(self):
+        '''Create all widgets on their initial state'''
         self.title_label = tk.Label(
             self.root,
             text="EchoTranslate",
-            font=("Roboto", 32),
-            fg="purple"
+            font=("Roboto", 40),
+            fg="#7857EC"
         )
         self.title_label.pack(pady=20)
 
-        self.record_button = tk.Button(
+        self.record_button = Button(
             self.root,
             text="Gravar",
-            command=self.start_recording,
+            command=self.on_click_start_recording,
             font=("Arial", 16),
-            bg="#006400",
-            fg="#ffffff",
-            relief=tk.RAISED,
-            activebackground="#008000",
+            # bg="#e31837",
+            # fg="#ffffff",
+            activebackground="#e31837",
             activeforeground="#ffffff",
+            relief=tk.RAISED,
             padx=20,
             pady=10
         )
         self.record_button.pack(pady=10)
 
-        self.stop_button = tk.Button(
+        self.stop_button = Button(
             self.root,
             text="Parar gravação",
             state=tk.DISABLED,
-            command=self.stop_recording,
+            command=self.on_click_stop_recording,
             font=("Arial", 16),
-            bg="#006400",
-            fg="#ffffff",
-            relief=tk.RAISED,
-            activebackground="#008000",
+            # bg="#e31837",
+            # fg="#ffffff",
+            activebackground="#e31837",
             activeforeground="#ffffff",
+            relief=tk.RAISED,
             padx=20,
             pady=10
         )
         self.stop_button.pack(pady=10)
 
-        self.play_audio_button = tk.Button(
+        self.play_audio_button = Button(
             self.root,
-            text="Tocar Áudio",
+            text="Ouvir áudio",
             state=tk.DISABLED,
-            command=self.play_audio,
+            command=self.on_click_play_audio,
             font=("Arial", 16),
-            bg="#006400",
-            fg="#ffffff",
-            relief=tk.RAISED,
-            activebackground="#008000",
+            # bg="#e31837",
+            # fg="#ffffff",
+            activebackground="#e31837",
             activeforeground="#ffffff",
+            relief=tk.RAISED,
             padx=20,
             pady=10
         )
@@ -72,12 +80,12 @@ class GUI:
 
         self.auto_play_audio = tk.Checkbutton(
             self.root,
-            text="Tocar áudio automaticamente",
+            text="Ouvir áudio automaticamente",
             command=self.activate_auto_play,
             font=("Arial", 16),
             fg="#ffffff",
             relief=tk.RAISED,
-            variable=self.auto_play_audio_bool,
+            variable=self.should_auto_play_audio,
             activeforeground="#ffffff",
             padx=20,
             pady=10,
@@ -87,39 +95,52 @@ class GUI:
         self.text_box = tk.Text(self.root, height=10, width=50)
         self.text_box.pack_forget()
 
-    def start_recording(self):
-        # Hide play button and invert buttons state
-        self.play_audio_button.pack_forget()
+    def on_click_start_recording(self):
+        '''Handle click on record button'''
+        # Hide play and text and invert buttons state
         self.text_box.pack_forget()
+        self.play_audio_button.pack_forget()
         self.record_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.ACTIVE)
-        self.translated_text = ""
         # Start recording
+        self.start_recording()
+
+    def on_click_stop_recording(self):
+        '''Handle click on stop recording button'''
+        self.stop_button.config(state=tk.DISABLED)
+        # Stop recording
+        self.stop_recording()
+        # Show results
+        time.sleep(1)
+        self.record_button.config(state=tk.ACTIVE)
+        self.play_audio_button.pack(pady=10)
+        self.show_text()
+        if self.should_auto_play_audio:
+            # Play audio if auto play is active
+            self.play_audio()
+        else:
+            self.play_audio_button.config(state=tk.ACTIVE)
+        
+
+    def on_click_play_audio(self):
+        '''Handle click on play audio button'''
+        self.play_audio_button.config(state=tk.DISABLED)
+        self.play_audio()
+        time.sleep(1)
+        self.play_audio_button.config(state=tk.ACTIVE)
+
+    def start_recording(self):
+        '''Starts recording from audio_recorder'''
+        self.translated_text = ""
         self.audio_recorder.start_recording()
 
     def stop_recording(self):
+        '''Stops recording from audio_recorder and saves it to a file'''
         self.audio_recorder.stop_recording()
         # Save recording
         filename = "recording.wav"
         self.audio_recorder.save_recording(filename)
         print(f"Arquivo de áudio: {filename}")
-        # Enable and disabled buttons
-        self.stop_button.config(state=tk.DISABLED)
-        self.record_button.config(state=tk.ACTIVE)
-        self.play_audio_button.pack(pady=10)
-        if self.auto_play_audio_bool:
-            self.play_audio()
-        else:
-            self.play_audio_button.config(state=tk.ACTIVE)
-        self.show_text()
-
-    def transcribe_text(self):
-        if self.translated_text != "":
-            return self.translated_text
-        text = self.transcriber.audio_to_text("recording.wav", "pt")
-        translated = self.transcriber.translate(text, "en")
-        self.translated_text = translated
-        return translated
 
     def show_text(self):
         text = self.transcribe_text()
@@ -130,16 +151,28 @@ class GUI:
         self.text_box.pack(pady=10)
         self.text_box.config(state=tk.DISABLED)
 
+    def transcribe_text(self):
+        '''Transcribes audio to text and translates it to selected language'''
+        if self.translated_text != "":
+            return self.translated_text
+        text = self.transcriber.audio_to_text("recording.wav", self.input_language)
+        translated = self.transcriber.translate(text, self.output_language)
+        self.translated_text = translated
+        return translated
+    
     def play_audio(self):
+        '''Plays audio from transcribed text'''
         text = self.transcribe_text()
-        filename = self.transcriber.text_to_audio(text, "translated_recording.mp3", 'en')
+        filename = self.transcriber.text_to_audio(text, "translated_recording.mp3", self.output_language)
         # Start the audio file
         os.system("mpg123 " + filename)
 
     def activate_auto_play(self):
-        self.auto_play_audio_bool = not self.auto_play_audio_bool
+        '''Activates auto play'''
+        self.should_auto_play_audio = not self.should_auto_play_audio
 
     def run(self):
+        '''Starts the GUI'''
         self.root.title("EchoTranslate")
         self.root.geometry("400x600")
         self.root.resizable(True, True)
