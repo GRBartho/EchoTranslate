5ed9cd8f2061f4e26a780b5d22185c5eac3a9df4
2viny <vinicius2003morh@gmail.com>
2023-06-26 01:08:34 -0300

    repo was glitchy
    it had 2 main's

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
-from src import *
-
+from core.audio.audio_recorder import AudioRecorder
+from core.gui.gui import GUI
+from core.transcribe.transcribe import Transcribe
+import tkinter as tk
 
 if __name__ == "__main__":
-    EchoApp()
+    root = tk.Tk()
+    audio_recorder = AudioRecorder()
+    transcriber = Transcribe()
+    app = GUI(root, audio_recorder, transcriber)
+    app.run()
+  
